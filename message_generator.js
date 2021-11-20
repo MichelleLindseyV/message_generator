2f6249fa59289ac9d0cff6d53fc2985bdc456e0f
Michelle <michellelverge@gmail.com>
2021-11-20 07:08:54 -0800

    moves message arrays into message object

--- message_generator.js
+++ message_generator.js
@@ -1,7 +1,10 @@
 //Message arrays:
 
-const youAre = ["enough", "strong", "wise", "capable", "resilient", "beautiful"];
-const youCan = ["handle this", "acheive your goals", "pick yourself up", "take up space", "forgive them", "choose to see the good"];
-const youWill = ["make great contributions", "create the life you desire", "let fo of what does not serve you", "not compare your journey to other's", "create change", "succeed today"];
+const messages = {
+  youAre: ["enough", "strong", "wise", "capable", "resilient", "beautiful"],
+  youCan: ["handle this", "acheive your goals", "pick yourself up", "take up space", "forgive them", "choose to see the good"],
+  youWill = ["make great contributions", "create the life you desire", "let fo of what does not serve you", "not compare your journey to other's", "create change", "succeed today"]
+}; 
 
 //Message structure: `You are ${youAre}. You can ${youCan}. You will ${youWill}.`
+
