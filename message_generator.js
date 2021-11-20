a3a8cc744b7d8e1d1e24e74cb93ff4f02bc5f66b
Michelle <michellelverge@gmail.com>
2021-11-20 07:02:42 -0800

    creates message arrays

--- message_generator.js
+++ message_generator.js
@@ -1,1 +1,6 @@
-console.log("I am working");
+//Message arrays:
+
+const youAre = ["enough", "strong", "wise", "capable", "resilient", "beautiful"];
+const youCan = ["handle this", "acheive your goals", "pick yourself up", "take up space", "forgive them", "choose to see the good"];
+const youWill = ["make great contributions", "create the life you desire", "let fo of what does not serve you", "not compare your journey to other's", "create change", "succeed today"];
+
